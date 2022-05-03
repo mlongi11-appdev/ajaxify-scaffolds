84b1097ae09bc1b37f370563e04141f792f0b13f
mlongi11 <mlongi@chicagobooth.edu>
2022-05-03 18:08:14 +0000

    attempting cards

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -53,6 +53,9 @@ class MoviesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to movies_url, notice: "Movie was successfully destroyed." }
       format.json { head :no_content }
+      format.js do
+        render template: "movies/destroy.js.erb"
+      end
     end
   end
 
