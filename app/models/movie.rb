e20187bf016c7bdf97f04973e9dceea0527bc7ad
mlongi11 <mlongi@chicagobooth.edu>
2022-05-03 17:46:53 +0000

    added belong tos

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  has_many :characters, dependent: :destroy
+
+  validates :title, presence: true
+  validates :director, presence: true
+  validates :year, presence: true
+  validates :duration, presence: true
+  validates :description, presence: true
+
+
 end
